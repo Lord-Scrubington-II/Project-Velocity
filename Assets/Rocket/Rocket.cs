1e5a61b09b333c4b978ef394c195641c1a2ed155
Zane Wang <zane.z.wang@gmail.com>
2020-08-09 01:51:06 -0700

    New Level, Code Improvements
    - added health mechanic WIP
    - added Lv. 2 geometry

--- Assets/Rocket/Rocket.cs
+++ Assets/Rocket/Rocket.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] public bool debug = true; //debug state
+    public bool debug = true; //debug state
 
     private Rigidbody collisionMesh;
     private AudioSource thrusterSound;
 
     [SerializeField] private float mainThrust = 18.0f;
     [SerializeField] private float rcsThrust = 200.0f;
-    private int fuel = 200;
+    [SerializeField] private bool useFuel = true;
+    [SerializeField] private int fuel = 1500;
+    [SerializeField] private int HP = 50;
+    [SerializeField] private int damageThreshold = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -32,13 +36,13 @@ public class Rocket : MonoBehaviour
     {
         if (debug)
         {
-            print("Rocket collided with " + " " + ".");
+            print("Rocket collided with " + collision.collider.ToString() + ".");
         }
 
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                //do nothing
+                //do nothing and/or win
                 if (debug)
                 {
                     print("Rocket hit a friendly object.");
@@ -54,29 +58,67 @@ public class Rocket : MonoBehaviour
 
                 break;
             default:
-                //death
-                if (debug)
+                //calc damage
+                if (CalcDamage(collision))
                 {
-                    print("Rocket collision caused death.");
+                    //death
                 }
-
                 break;
         }
     }
 
+    private bool CalcDamage(Collision collision)
+    {
+        //TODO: test this!
+
+        //get rocket's relative velocity on collision with generic obstacles.
+        int velocity = (int)Math.Floor(collision.relativeVelocity.magnitude);
+        
+        //rocket go boom
+        bool death = false;
+
+        if (velocity > damageThreshold)
+        {
+            HP -= velocity - damageThreshold;
+
+            if (debug)
+            {
+                print("Rocket collided with an unfriendly object. HP Remaining: " + HP);
+
+                if (HP <= 0)
+                {
+                    print("Rocket collision caused death.");
+                }
+            }
+
+            if (HP <= 0)
+            {
+                death = true;
+            }
+        }
+        return death;
+    }
+
     private void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && fuel > 0)
         {
             //activate forward thrust
             collisionMesh.AddRelativeForce(mainThrust * Vector3.up);
+            if (useFuel)
+            {
+                fuel--;
+            }
+
+            //soundfx - thrust
             if (!thrusterSound.isPlaying)
             {
                 thrusterSound.Play();
             }
+
             if (debug)
             {
-                print("Rocket is thrusting.");
+                print("Rocket is thrusting. Fuel remaining: " + fuel + " units.");
             }
         }
         else
@@ -90,7 +132,7 @@ public class Rocket : MonoBehaviour
         collisionMesh.freezeRotation = true; //all control of rotation is manual.
 
 
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        float rotationThisFrame = rcsThrust * Time.deltaTime; //ensure that RPM is always same.
 
         if (Input.GetKey(KeyCode.A))
         {
