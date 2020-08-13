5fea7701620a4a82aa432fd1c3f22b6cfb0200ba
Zane Wang <zane.z.wang@gmail.com>
2020-08-12 21:35:11 -0700

    Various Improvements
    - Reorganized Assets
    - Added 3rd Level
    - Added Oscillators

---
+++ Assets/Geometry/Oscillator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10.0f, 10.0f, 10.0f);
+    [SerializeField] float period = 2.0f;
+    private float movementFactor; // 0 for not moved, -1 or 1 for fully moved.
+
+    [Range(0, 2)]
+    [SerializeField]
+    private float oscillatorVelocityFactor = 0.8f;
+
+    private Vector3 initPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = this.transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period == Mathf.Epsilon) return;
+        float cycles = (Time.time / period) * oscillatorVelocityFactor; //grows from 0 through runtime
+
+        const float tau = 2.0f * Mathf.PI;
+        float rawSineMagnitude = Mathf.Sin(cycles * tau);
+
+        //for moving obstacles that init one extreme of their oscillation paths
+        //movementFactor = rawSineMagnitude / 2.0f + 0.5f;
+
+        //for moving obstacles that init at the centre of their oscillation paths
+        movementFactor = rawSineMagnitude;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = initPos + offset;
+    }
+}
--- Assets/Rocket/Rocket.cs
+++ Assets/Rocket/Rocket.cs
@@ -3,37 +3,68 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     public bool debug = true; //debug state
 
-    private Rigidbody collisionMesh;
-    private AudioSource thrusterSound;
+    private Rigidbody rigidBody;
+    private AudioSource rocketAudio;
 
-    [SerializeField] private float mainThrust = 18.0f;
+    [SerializeField] private float mainThrust = 1000.0f;
     [SerializeField] private float rcsThrust = 200.0f;
+    [SerializeField] private float levelLoadDelay = 4.0f;
+
+
     [SerializeField] private bool useFuel = true;
     [SerializeField] private int fuel = 1500;
-    [SerializeField] private int HP = 50;
+    [SerializeField] private int HP = 20;
     [SerializeField] private int damageThreshold = 3;
+    [SerializeField] State state = State.Alive;
+
+    [SerializeField] private AudioClip mainEngine;
+    [SerializeField] private AudioClip deathNoise;
+    [SerializeField] private AudioClip goalReached;
+
+    [SerializeField] private ParticleSystem mainEngineParticles;
+    [SerializeField] private ParticleSystem deathParticles;
+    [SerializeField] private ParticleSystem goalReachedParticles;
+
+    private static int nextSceneIndex = 1;
+    private static readonly int sceneCount = 3;
+
+    enum State { Alive, Dying, Transitioning }
 
     // Start is called before the first frame update
     void Start()
     {
-        collisionMesh = this.GetComponent<Rigidbody>();
-        thrusterSound = this.GetComponent<AudioSource>();
+        rigidBody = this.GetComponent<Rigidbody>();
+        rocketAudio = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Gyrate();
+        if(state == State.Alive)
+        {
+            HandleThrust();
+            HandleGyration();
+        }
+
+        //todo consider removal
+        if(state == State.Transitioning)
+        {
+            //stop physics engine from controlling rocket after target reached.
+            rigidBody.freezeRotation = true;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        if (state != State.Alive) { return; }
+
         if (debug)
         {
             print("Rocket collided with " + collision.collider.ToString() + ".");
@@ -47,8 +78,8 @@ public class Rocket : MonoBehaviour
                 {
                     print("Rocket hit a friendly object.");
                 }
-
                 break;
+
             case "Fuel":
                 //replenish fuel
                 if (debug)
@@ -57,24 +88,58 @@ public class Rocket : MonoBehaviour
                 }
 
                 break;
+
+            case "Target":
+                //next level!
+                if (debug)
+                {
+                    print("Rocket landed!");
+                }
+                state = State.Transitioning;
+                Invoke("LoadNextLevel", levelLoadDelay);
+                HandleAudioEvent(goalReached);
+                ClearParticles();
+                goalReachedParticles.Play();
+                break;
+
             default:
                 //calc damage
                 if (CalcDamage(collision))
                 {
                     //death
+                    state = State.Dying;
+                    Invoke("Restart", levelLoadDelay);
+                    HandleAudioEvent(deathNoise);
+                    ClearParticles();
+                    deathParticles.Play();
                 }
                 break;
         }
     }
+    
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+        
+        if(nextSceneIndex < sceneCount)
+        {
+            nextSceneIndex++;
+        }
+    }
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+        nextSceneIndex = 1;
+    }
 
     private bool CalcDamage(Collision collision)
     {
-        //TODO: test this!
+        //todo test this!
 
         //get rocket's relative velocity on collision with generic obstacles.
         int velocity = (int)Math.Floor(collision.relativeVelocity.magnitude);
         
-        //rocket go boom
+        //rocket go boom?
         bool death = false;
 
         if (velocity > damageThreshold)
@@ -91,6 +156,7 @@ public class Rocket : MonoBehaviour
                 }
             }
 
+            //rocket go boom
             if (HP <= 0)
             {
                 death = true;
@@ -99,46 +165,64 @@ public class Rocket : MonoBehaviour
         return death;
     }
 
-    private void Thrust()
+    private void HandleThrust()
     {
         if (Input.GetKey(KeyCode.Space) && fuel > 0)
         {
-            //activate forward thrust
-            collisionMesh.AddRelativeForce(mainThrust * Vector3.up);
-            if (useFuel)
-            {
-                fuel--;
-            }
-
-            //soundfx - thrust
-            if (!thrusterSound.isPlaying)
-            {
-                thrusterSound.Play();
-            }
-
-            if (debug)
-            {
-                print("Rocket is thrusting. Fuel remaining: " + fuel + " units.");
-            }
+            DoThrust();
         }
         else
         {
-            thrusterSound.Stop();
+            rocketAudio.Stop();
+            mainEngineParticles.Stop();
         }
     }
 
-    private void Gyrate()
+    private void DoThrust()
     {
-        collisionMesh.freezeRotation = true; //all control of rotation is manual.
+        //activate forward thrust
+        rigidBody.AddRelativeForce(mainThrust * Vector3.up * Time.deltaTime);
+        if (useFuel)
+        {
+            fuel--;
+        }
+
+        //soundfx - thrust
+        if (!rocketAudio.isPlaying)
+        {
+            HandleAudioEvent(mainEngine);
+        }
+
+        if (!mainEngineParticles.isEmitting)
+        {
+            mainEngineParticles.Play();
+        }
+
+        if (debug)
+        {
+            print("Rocket is thrusting. Fuel remaining: " + fuel + " units.");
+        }
+    }
+
+    private void HandleGyration()
+    {
+        rigidBody.freezeRotation = true; //all control of rotation is manual.
 
 
         float rotationThisFrame = rcsThrust * Time.deltaTime; //ensure that RPM is always same.
 
+        Gyrate(rotationThisFrame);
+
+        rigidBody.freezeRotation = false; //resume physics
+    }
+
+    private void Gyrate(float rotationThisFrame)
+    {
         if (Input.GetKey(KeyCode.A))
         {
             //gyrate left
             transform.Rotate(Vector3.forward * rotationThisFrame);
-            
+
             if (debug)
             {
                 print("Rocket is rotating left.");
@@ -148,13 +232,24 @@ public class Rocket : MonoBehaviour
         {
             //gyrate right
             transform.Rotate(Vector3.back * rotationThisFrame);
-           
+
             if (debug)
             {
                 print("Rocket is rotating right.");
             }
         }
+    }
 
-        collisionMesh.freezeRotation = false; //resume physics
+    private void HandleAudioEvent(AudioClip clip)
+    {
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(clip);
+    }
+
+    private void ClearParticles()
+    {
+        mainEngineParticles.Stop();
+        deathParticles.Stop();
+        goalReachedParticles.Stop();
     }
 }
